59ca00629e0baf2bf59fdf2be591968a748354c7
ayoub01 <elotmaniayoub@gmail.com>
2016-04-17 16:38:26 +0100

    Update etats_financiers.py

--- etats_financiers.py
+++ etats_financiers.py
@@ -14,33 +14,4 @@ class etats_financiers(osv.Model):
     }
    
     
-    def init(self, cr):
-        tools.drop_table_if_exists(cr, 'analyse_balance')
-        cr.execute("""
-                           INSERT INTO analyse_balance VALUES (
-                                 
-                           select  
-                                    account_account.code , 
-                                    account_journal.default_debit_account_id ,
-                                    account_journal.default_credit_account_id ,
-                                    account_move_line.debit ,
-                                    account_move_line.credit ,
-                                    CASE WHEN account_move.balance>0 THEN 'Debit'
-                                    ELSE 'Credit'
-                                    END AS balance,
-                                   account_account.type 
-                                  
-                            FROM 
-                                public.account_move_line INNER JOIN public.account_account
-                                    ON public.account_move_line.account_id = public.account_account.id
-                                INNER JOIN public.account_journal
-                                    ON public.account_move_line.period_id = public.account_journal.id
-                                INNER JOIN public.account_move
-                                    ON account_account.id = public.account_move.id
-                                
-                            GROUP BY account_account.code, account_account.type, account_journal.default_debit_account_id, account_journal.default_credit_account_id, account_move_line.debit, account_move_line.credit, account_move.balance 
-                            ORDER BY account_account.code
-                            
-                            )""")
-
    
